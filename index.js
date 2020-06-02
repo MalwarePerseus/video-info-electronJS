7d1ca053834e7ddef755a4798b4324b26df665ff
MalwarePerseus <malwareperseus@gmail.com>
2020-06-02 18:46:10 +0530

    Final Commit.

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow , ipcMain } = electron;
+const ffmpeg = require('fluent-ffmpeg');
+let mainWindow;
 
 app.on('ready', () => {
-	const mainWindow = new BrowserWindow({});	
+	mainWindow = new BrowserWindow({
+		webPreferences: {
+			nodeIntegration : true
+		}
+	});	
 	//mainWindow.loadURL(`file:/${__dirname}/index.html`);
 	mainWindow.loadFile('index.html')
 });
+
+ipcMain.on('video:submit', (event, path) =>{
+	ffmpeg.ffprobe(path, (err, metadata) =>{
+		// console.log("Video duration is : " , metadata.format.duration);
+		mainWindow.webContents.send('video:metadata', metadata.format.duration);
+	});
+});
+ 
